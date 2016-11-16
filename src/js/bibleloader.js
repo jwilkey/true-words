54b33bc1bbcba2747d81c7cc1e48178942367f82
Justin Wilkey <justin.wilkey@tentkeep.com>
2016-11-16 12:50:17 -0500

    Update vue-cli, fix build files

--- src/js/bibleloader.js
+++ src/js/bibleloader.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 export default {
   load (bookIdentifier, chapter, onload) {
-    var path = '../../static/bibles/nasb/' + bookIdentifier + '/' + chapter + '.json'
+    var path = '/static/bibles/nasb/' + bookIdentifier + '/' + chapter + '.json'
     $.getJSON(path, function (json) {
       onload(json)
     })
