770e0cb23ca869298336475692c88acc9af08306
Justin Wilkey <justin.wilkey@tentkeep.com>
2017-02-27 12:38:55 -0500

    Add StewarshipHelp

--- src/components/help/StewardshipHelp.vue
+++ src/components/help/StewardshipHelp.vue
@@ -3,7 +3,11 @@
     <div class="clearfix">
       <p><i>When applying the Scripture, be specific and be personal.</i></p>
       <hr />
-      <p>To complete this activity, answer the questions at the bottom of the screen.</p>
+      <p>To complete this activity, answer the questions as they appear. This activity focuses on four main "resources" in our life: <strong>time, money, thoughts, and relationships.</strong></p>
+      <br />
+      <p>The first set of questions will help you think about your life context right now. These things should be on your mind as you apply the scripture passage to your life.</p>
+      <br />
+      <p>The second set of questions will help you examine these areas of your life. You must make decisions as to how you prioritize and spend these resources. You will answer how the passage does or does not affect each of these areas of your life.</p>
     </div>
   </div>
 </template>
